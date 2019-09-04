ff96141ac560fb35f1bc2290ee402c570c3759a2
Andy DuBois <andydubois89@gmail.com>
2019-09-03 22:42:34 -0500

    halfway through tutorial, 2 sections into validation header

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
-
-//Components
 import AddressSuggest from "../AddressSuggest/AddressSuggest";
 import AddressInput from "../AddressInput/AddressInput";
+import axios from "axios";
+require("dotenv").config();
+//Components
 
 class AddressForm extends Component {
   constructor(props) {
     super(props);
 
-    const address = this.getEmptyAddress();
     this.state = {
       address: address,
       query: "",
       locationId: ""
     };
 
+    // User has entered something in address suggest field
     this.onQuery = this.onQuery.bind(this);
+    // User has entered something in address field
+    this.onAddressChange = this.onAddressChange.bind(this);
+    // User has clicked the check button
+    this.onCheck = this.onCheck.bind(this);
+    // User has clicked the clear button
+    this.onClear = this.onClear.bind(this);
   }
 
   onQuery(evt) {
@@ -33,8 +40,8 @@ class AddressForm extends Component {
     axios
       .get("https://autocomplete.geocoder.api.here.com/6.2/suggest.json", {
         params: {
-          app_id: APP_ID_HERE,
-          app_code: APP_CODE_HERE,
+          app_id: process.env.APP_ID_HERE,
+          app_code: process.env.APP_CODE_HERE,
           query: query,
           maxresults: 1
         }
@@ -50,24 +57,29 @@ class AddressForm extends Component {
       });
   }
 
-
   render() {
     return (
-      <div className='container'>
-        <AddressSuggest />
-        query="4168 S" />
+      <div class='container'>
+        <AddressSuggest query={this.state.query} onChange={this.onQuery} />
         <AddressInput
-          street='4168 Shattuck Ave'
-          city='Berkeley'
-          state='CA'
-          code='94704'
-          country='USA'
+          street={this.state.address.street}
+          city={this.state.address.city}
+          state={this.state.address.state}
+          postalCode={this.state.address.postalCode}
+          country={this.state.address.country}
         />
         <br />
-        <button type='submit' className='btn btn-primary'>
+        {/* {result} */}
+        <button
+          type='submit'
+          className='btn btn-primary'
+          onClick={this.onCheck}>
           Check
         </button>
-        <button type='submit' className='btn btn-outline-secondary'>
+        <button
+          type='submit'
+          className='btn btn-outline-secondary'
+          onClick={this.onClear}>
           Clear
         </button>
       </div>
